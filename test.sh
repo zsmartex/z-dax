34a1a9f9734ac7d6e34b3a33bbb1d0513e0b6f26
Hữu Hà ZSmart <40519768+huuhait@users.noreply.github.com>
2020-02-04 23:01:59 +0700

    add volumes mount for backend services

--- test.sh
+++ test.sh
@@ -1,4 +1,6 @@
-docker run --rm -itd --name nfs --privileged -v /root/project:/nfs.1 -e SHARED_DIRECTORY=/nfs.1 -p 0.0.0.0:2049:2049 itsthenetwork/nfs-server-alpine:latest
+sudo mount -t vboxsf -o rw,uid=1000,gid=1000,dmode=755,fmode=644 z-dax ~/microkube
+
+docker run --rm -itd --name nfs --privileged -v /home/app/project:/nfs.1 -e SHARED_DIRECTORY=/nfs.1 -p 0.0.0.0:2049:2049 itsthenetwork/nfs-server-alpine:latest
 
 docker stack deploy --with-registry-auth
 
