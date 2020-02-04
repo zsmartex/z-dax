bb61ed85bba7073a224d075440d0935d907af92d
Hữu Hà ZSmart <40519768+huuhait@users.noreply.github.com>
2020-02-05 01:07:00 +0700

    remove Dockerfile, change wrong volume bind folder

--- test.sh
+++
@@ -1,6 +0,0 @@
-sudo mount -t vboxsf -o rw,uid=1000,gid=1000,dmode=755,fmode=644 z-dax ~/microkube
-
-docker run --rm -itd --name nfs --privileged -v /home/app/project:/nfs.1 -e SHARED_DIRECTORY=/nfs.1 -p 0.0.0.0:2049:2049 itsthenetwork/nfs-server-alpine:latest
-
-docker stack deploy --with-registry-auth
-
