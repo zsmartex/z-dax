fe40bb645d6afedf2d53d666cfbd5b14766fcead
Hữu Hà <business@zsmart.tech>
2020-04-13 12:46:59 +0700

    Enchantment: traefik 2.0, remove postmaster, convert dos to unix

--- bin/dev.sh
+++ bin/dev.sh
@@ -19,3 +19,26 @@ cp -v *.pem ~/.docker
 ExecStart=
 ExecStart=/usr/bin/dockerd --tlsverify --tlscacert=/home/huuhadz2k/.docker/ca.pem --tlscert=/home/huuhadz2k/.docker/server-cert.pem --tlskey=/home/huuhadz2k/.docker/server-key.pem \
   -H=0.0.0.0:2376
+
+docker run \
+  -v /home/huuhadz2k/nfs_share:/nfs \
+  -v /home/huuhadz2k/test/server.keytab:/etc/krb5.keytab:ro \
+  -v /home/huuhadz2k/test/server.krb5conf:/etc/krb5.conf:ro \
+  -e NFS_ENABLE_KERBEROS=1 \
+  --hostname my-nfs-server.com \
+  -e NFS_EXPORT_0='/nfs                 *(ro,no_subtree_check)' \
+  --cap-add SYS_ADMIN \
+  -p 2049:2049 \
+  erichough/nfs-server
+
+docker run                                                  \
+  -v /home/app/nfs_share:/nfs                         \
+  -e NFS_EXPORT_0='/nfs                 *(ro,no_subtree_check)' \
+  -e NFS_ENABLE_KERBEROS=1                                  \
+  --hostname my-nfs-server.com                              \
+  --cap-add SYS_ADMIN                                       \
+  -p 2049:2049                                              \
+  erichough/nfs-server
+
+# Change all file type from dos to unix
+find . -type f -exec dos2unix {} \;
